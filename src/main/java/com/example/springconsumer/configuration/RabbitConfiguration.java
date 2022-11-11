d9f45a2fc68ea2dd00c642bbb9bffe88882c5c6c
Mario Franco <mario.franco@wdev.int>
2022-11-11 10:44:20 -0300

    testando configuracao de consumer

--- src/main/java/com/example/springconsumer/configuration/RabbitConfiguration.java
+++ src/main/java/com/example/springconsumer/configuration/RabbitConfiguration.java
@@ -1,6 +1,9 @@
 package com.example.springconsumer.configuration;
 
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -11,5 +14,14 @@ public class RabbitConfiguration {
     @Bean
     public MessageConverter jsonConvert() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory factory(ConnectionFactory connectionFactory,
+                                                        SimpleRabbitListenerContainerFactoryConfigurer configurer) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory,connectionFactory);
+        factory.setMessageConverter(jsonConvert());
+        return factory;
     }
 }
---
+++ src/main/java/com/example/springconsumer/service/implementation/ConsumerServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.springconsumer.service.implementation;
+
+import com.example.springconsumer.dto.Message;
+import com.example.springconsumer.service.ConsumerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerServiceImpl implements ConsumerService {
+    @Override
+    public void action(Message message) {
+        System.out.println(message.getText());
+    }
+}
